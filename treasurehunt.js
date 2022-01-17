bb8434ef814413b360692f0c161475f1cc109805
Yasimcd <91527193+Yasimcd@users.noreply.github.com>
2022-01-16 16:30:16 -0800

    Add files via upload

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,18 +1,25 @@
 var trea = Math.floor(Math.random() *12)
 var bomb = Math.floor(Math.random() *12)
-var tree = Math.floor(Math.random() *12)
 
+while (trea === bomb) {
+  bomb = Math.floor(Math.random() *12)
+}
+console.log("trea", trea);
+console.log("bomb", bomb);
 const treasure = (n) =>{
-    if (n === trea) {
+
+
+  if (n == trea) {
     document.getElementById(n).innerHTML="&#x1f308"
-    alert("You win!!!")
-    document.getElementById(n).innerHTML= herf= ("https://www.pngkey.com/png/detail/548-5481911_treasure-hunt-chest-items.png");
+    document.getElementById("game-board").innerHTML='<img src="treasurebox.png"/>'
+
   }
   else if (n === bomb) {
     document.getElementById(n).innerHTML="&#x2620"
-    alert("Game Over.")
+    document.getElementById("game-board").innerHTML='<img src="skull.png"/>'
   }
   else {
     document.getElementById(n).innerHTML="&#x1f332"
   }
+
 }
